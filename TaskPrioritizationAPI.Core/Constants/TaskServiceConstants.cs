6407f2217c87885539b7eb655f22b4ad3974b35a
MaximVanchev <maxim.van.mv@gmail.com>
2025-02-10 03:05:26 +0200

    Adding core constants and contracts.

---
+++ TaskPrioritizationAPI.Core/Constants/TaskServiceConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskPrioritizationAPI.Core.Constants
+{
+    public class TaskServiceConstants
+    {
+        public const int Task_High_Priority_Day_Minimum = 2;
+    }
+}
---
+++ TaskPrioritizationAPI.Core/Contracts/ITaskService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskPrioritizationAPI.Core.Models;
+using TaskPrioritizationAPI.Infrastructure.Data.Common;
+
+namespace TaskPrioritizationAPI.Core.Contracts
+{
+    public interface ITaskService
+    {
+        Task<IEnumerable<TaskViewModel>> GetAllTasksByPriority();
+
+        Task<IEnumerable<TaskViewModel>> GetAllUncompleatedTasks();
+
+        Task<TaskViewModel> AddTask(TaskAddViewModel taskAddModel);
+
+        Task<bool> RemoveTask(Guid taskId);
+
+        Task<TaskViewModel> GetTaskById(Guid taskId);
+
+        Task<bool> UpgateTask(TaskViewModel taskViewModel);
+
+        Task<PriorityLevel> PriorityCalculator(string dueDate , bool isCritical, bool isCompleated);
+    }
+}
