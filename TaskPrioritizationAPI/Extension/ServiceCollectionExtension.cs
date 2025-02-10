500426fde77f93df5f0e5f2192849f6a22e2d99b
MaximVanchev <maxim.van.mv@gmail.com>
2025-02-10 06:20:02 +0200

    Adding service collection Extension.

---
+++ TaskPrioritizationAPI/Extension/ServiceCollectionExtension.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TaskPrioritizationAPI.Core.Contracts;
+using TaskPrioritizationAPI.Core.Services;
+using TaskPrioritizationAPI.Infrastructure.Data;
+using TaskPrioritizationAPI.Infrastructure.Data.Repositories;
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IApplicationDbRepository, ApplicationDbRepository>();
+            services.AddScoped<ITaskService, TaskService>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationDbContexts(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<Context>(options =>
+                options.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
--- TaskPrioritizationAPI/Program.cs
+++ TaskPrioritizationAPI/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationDbContexts(builder.Configuration);
+builder.Services.AddApplicationServices();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
